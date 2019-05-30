7652423cab2ae8568489b4e0a5766b44759ef19d
suralei <375262986@qq.com>
2019-05-30 13:26:55 +0800

    修改时区、数据库

--- config/web.php
+++ config/web.php
@@ -4,6 +4,8 @@ $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 
 $config = [
+    'language' => 'zh-CN',
+    'timeZone' => 'Asia/Shanghai',
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
